2fdbc06b0d418d4c756bed2bef08982b45e6c6d5
venyz <contact@ventsislavzhechev.eu>
2014-04-14 18:05:39 +0200

    Updated engine deployment. Improved deployMTEngines

--- deployMTEngines.pl
+++ deployMTEngines.pl
@@ -4,6 +4,10 @@
 # Created on 11 Feb 2014 by Ventsislav Zhechev
 #
 # ChangeLog
+# v0.2			Modified on 14 Apr 2014 by Ventsislav Zhechev
+# Updated engine list.
+# Modified the deployment algorithm to make sure each target server is only connected from one thread at a time.
+#
 # v0.1.2		Modified on 19 Mar 2014 by Ventsislav Zhechev
 # Updated engine list.
 #
@@ -20,6 +24,7 @@ use strict;
 use utf8;
 
 use threads;
+use threads::shared;
 use Thread::Queue;
 
 use List::Util qw/shuffle/;
@@ -35,43 +40,45 @@ $| = 1;
 
 
 my %systems = (
-"ussclpdapcmsl01" => ["fy14_RU-EN_a","fy15_EN-DA_c","fy15_EN-EN_UK_b","fy15_EN-FR_b","fy15_EN-HU_c","fy15_EN-NO_c","fy15_EN-PL_c","fy15_EN-VI_b","fy14_XX-EN_a",],
-"ussclpdapcmsl02" => ["fy14_DE-EN_a","fy15_EN-ES_b","fy15_EN-JP_b","fy15_EN-SV_c","fy14_XX-EN_a",],
-"ussclpdapcmsl03" => ["fy14_KO-EN_a","fy15_EN-IT_c","fy15_EN-NL_c","fy15_EN-RU_c","fy15_EN-ZH_HANS_b","fy14_XX-EN_a",],
-"ussclpdapcmsl04" => ["fy14_IT-EN_a","fy14_JP-EN_a","fy15_EN-CS_c","fy15_EN-FI_c","fy14_XX-EN_a",],
-"ussclpdapcmsl05" => ["fy14_ES-EN_a","fy14_PL-EN_a","fy15_EN-ES_b","fy15_EN-PT_BR_c","fy14_XX-EN_a",],
-"ussclpdapcmsl06" => ["fy14_ZH_HANT-EN_a","fy15_EN-CS_c","fy15_EN-DE_c","fy15_EN-ZH_HANT_b","fy14_XX-EN_a",],
-"ussclpdapcmsl07" => ["fy15_EN-DE_c","fy15_EN-IT_c","fy15_EN-KO_b","fy15_EN-PL_c","fy15_EN-ZH_HANT_b",],
-"ussclpdapcmsl08" => ["fy15_EN-DE_c","fy15_EN-IT_c","fy15_EN-KO_b","fy15_EN-PL_c","fy15_EN-ZH_HANT_b",],
-"ussclpdapcmsl09" => ["fy15_EN-DE_c","fy15_EN-IT_c","fy15_EN-KO_b","fy15_EN-PL_c","fy15_EN-ZH_HANT_b",],
-"ussclpdapcmsl10" => ["fy15_EN-DE_c","fy15_EN-IT_c","fy15_EN-KO_b","fy15_EN-PL_c","fy15_EN-ZH_HANT_b",],
-"ussclpdapcmsl11" => ["fy15_EN-DE_c","fy15_EN-IT_c","fy15_EN-KO_b","fy15_EN-PL_c","fy15_EN-ZH_HANT_b",],
-"ussclpdapcmsl12" => ["fy15_EN-DE_c","fy15_EN-IT_c","fy15_EN-KO_b","fy15_EN-PL_c","fy15_EN-ZH_HANT_b",],
-"ussclpdmtlnx001" => ["fy15_EN-DE_c","fy15_EN-IT_c","fy15_EN-KO_b","fy15_EN-PL_c","fy15_EN-ZH_HANT_b",],
-"ussclpdmtlnx002" => ["fy14_FR-EN_a","fy14_PT_BR-EN_a","fy15_EN-ES_b","fy15_EN-PT_BR_c","fy14_XX-EN_a",],
-"ussclpdmtlnx003" => ["fy14_ZH_HANS-EN_a","fy15_EN-CS_c","fy15_EN-FR_b","fy15_EN-KO_b","fy14_XX-EN_a",],
-"ussclpdmtlnx004" => ["fy15_EN-FR_b","fy15_EN-HU_c","fy15_EN-JP_b","fy15_EN-RU_c",],
-"ussclpdmtlnx005" => ["fy15_EN-FR_b","fy15_EN-HU_c","fy15_EN-JP_b","fy15_EN-RU_c",],
-"ussclpdmtlnx006" => ["fy15_EN-FR_b","fy15_EN-HU_c","fy15_EN-JP_b","fy15_EN-RU_c",],
-"ussclpdmtlnx007" => ["fy15_EN-FR_b","fy15_EN-HU_c","fy15_EN-JP_b","fy15_EN-RU_c",],
-"ussclpdmtlnx008" => ["fy15_EN-FR_b","fy15_EN-HU_c","fy15_EN-JP_b","fy15_EN-RU_c",],
-"ussclpdmtlnx009" => ["fy15_EN-FR_b","fy15_EN-HU_c","fy15_EN-JP_b","fy15_EN-RU_c",],
-"ussclpdmtlnx010" => ["fy15_EN-JP_b","fy15_EN-PT_BR_c","fy15_EN-RU_c","fy15_EN-ZH_HANS_b",],
-"ussclpdmtlnx011" => ["fy15_EN-CS_c","fy15_EN-ES_b","fy15_EN-HU_c","fy15_EN-JP_b","fy15_EN-PT_BR_c",],
-"ussclpdmtlnx012" => ["fy15_EN-DE_c","fy15_EN-JP_b","fy15_EN-ZH_HANS_b",],
-"mtprd01" => ["fy14_HU-EN_a","fy14_XX-EN_a",],
-"mtprd02" => ["fy14_CS-EN_a","fy14_XX-EN_a",],
-"mtprd03" => ["fy15_EN-PT_BR_c","fy15_EN-ZH_HANS_b",],
-"mtprd04" => ["fy15_EN-PT_BR_c","fy15_EN-ZH_HANS_b",],
-"mtprd05" => ["fy15_EN-PT_BR_c","fy15_EN-ZH_HANS_b",],
-"mtprd06" => ["fy15_EN-PT_BR_c","fy15_EN-ZH_HANS_b",],
-"mtprd07" => ["fy15_EN-PT_BR_c","fy15_EN-ZH_HANS_b",],
-"mtprd08" => ["fy15_EN-PT_BR_c","fy15_EN-ZH_HANS_b",],
-"mtprd09" => ["fy15_EN-CS_c","fy15_EN-ES_b",],
-"mtprd10" => ["fy15_EN-CS_c","fy15_EN-ES_b",],
-"mtprd11" => ["fy15_EN-CS_c","fy15_EN-ES_b",],
-"mtprd12" => ["fy15_EN-CS_c","fy15_EN-ES_b",],
+"ussclpdapcmsl01" => ["fy15_EN-DA_d","fy15_EN-EN_GB_a","fy15_EN-FR_c","fy15_EN-HU_d","fy15_EN-NO_d","fy15_EN-PL_d","fy15_EN-VI_c","fy15_RU-EN_a","fy15_XX-EN_b",],
+"ussclpdapcmsl02" => ["fy15_DE-EN_a","fy15_EN-ES_c","fy15_EN-JP_c","fy15_EN-SV_d","fy15_XX-EN_b",],
+"ussclpdapcmsl03" => ["fy15_EN-IT_d","fy15_EN-NL_d","fy15_EN-RU_d","fy15_EN-ZH_HANS_c","fy15_KO-EN_a","fy15_XX-EN_b",],
+"ussclpdapcmsl04" => ["fy15_EN-CS_d","fy15_EN-FI_d","fy15_IT-EN_a","fy15_JP-EN_a","fy15_XX-EN_b",],
+"ussclpdapcmsl05" => ["fy15_EN-ES_c","fy15_EN-PT_BR_d","fy15_ES-EN_a","fy15_PL-EN_a","fy15_XX-EN_b",],
+"ussclpdapcmsl06" => ["fy15_EN-CS_d","fy15_EN-DE_d","fy15_EN-ZH_HANT_c","fy15_ZH_HANT-EN_a","fy15_XX-EN_b",],
+"ussclpdapcmsl07" => ["fy15_EN-DE_d","fy15_EN-IT_d","fy15_EN-KO_c","fy15_EN-PL_d","fy15_EN-ZH_HANT_c",],
+"ussclpdapcmsl08" => ["fy15_EN-DE_d","fy15_EN-IT_d","fy15_EN-KO_c","fy15_EN-PL_d","fy15_EN-ZH_HANT_c",],
+"ussclpdapcmsl09" => ["fy15_EN-DE_d","fy15_EN-IT_d","fy15_EN-KO_c","fy15_EN-PL_d","fy15_EN-ZH_HANT_c",],
+"ussclpdapcmsl10" => ["fy15_EN-DE_d","fy15_EN-IT_d","fy15_EN-KO_c","fy15_EN-PL_d","fy15_EN-ZH_HANT_c",],
+"ussclpdapcmsl11" => ["fy15_EN-DE_d","fy15_EN-IT_d","fy15_EN-KO_c","fy15_EN-PL_d","fy15_EN-ZH_HANT_c",],
+"ussclpdapcmsl12" => ["fy15_EN-DE_d","fy15_EN-IT_d","fy15_EN-KO_c","fy15_EN-PL_d","fy15_EN-ZH_HANT_c",],
+"ussclpdmtlnx001" => ["fy15_EN-DE_d","fy15_EN-IT_d","fy15_EN-KO_c","fy15_EN-PL_d","fy15_EN-ZH_HANT_c",],
+"ussclpdmtlnx002" => ["fy15_EN-ES_c","fy15_EN-PT_BR_d","fy15_FR-EN_a","fy15_PT_BR-EN_a","fy15_XX-EN_b",],
+"ussclpdmtlnx003" => ["fy15_EN-CS_d","fy15_EN-FR_c","fy15_EN-KO_c","fy15_ZH_HANS-EN_a","fy15_XX-EN_b",],
+"ussclpdmtlnx004" => ["fy15_EN-FR_c","fy15_EN-HU_d","fy15_EN-JP_c","fy15_EN-RU_d",],
+"ussclpdmtlnx005" => ["fy15_EN-FR_c","fy15_EN-HU_d","fy15_EN-JP_c","fy15_EN-RU_d",],
+"ussclpdmtlnx006" => ["fy15_EN-FR_c","fy15_EN-HU_d","fy15_EN-JP_c","fy15_EN-RU_d",],
+"ussclpdmtlnx007" => ["fy15_EN-FR_c","fy15_EN-HU_d","fy15_EN-JP_c","fy15_EN-RU_d",],
+"ussclpdmtlnx008" => ["fy15_EN-FR_c","fy15_EN-HU_d","fy15_EN-JP_c","fy15_EN-RU_d",],
+"ussclpdmtlnx009" => ["fy15_EN-FR_c","fy15_EN-HU_d","fy15_EN-JP_c","fy15_EN-RU_d",],
+"ussclpdmtlnx010" => ["fy15_EN-JP_c","fy15_EN-PT_BR_d","fy15_EN-RU_d","fy15_EN-ZH_HANS_c",],
+"ussclpdmtlnx011" => ["fy15_EN-CS_d","fy15_EN-ES_c","fy15_EN-HU_d","fy15_EN-JP_c","fy15_EN-PT_BR_d",],
+"ussclpdmtlnx012" => ["fy15_EN-DE_d","fy15_EN-JP_c","fy15_EN-ZH_HANS_c",],
+"mtprd01" => ["fy15_HU-EN_a","fy15_XX-EN_b",],
+"mtprd02" => ["fy15_CS-EN_a","fy15_XX-EN_b",],
+"mtprd03" => ["fy15_EN-PT_BR_d","fy15_EN-ZH_HANS_c",],
+"mtprd04" => ["fy15_EN-PT_BR_d","fy15_EN-ZH_HANS_c",],
+"mtprd05" => ["fy15_EN-PT_BR_d","fy15_EN-ZH_HANS_c",],
+"mtprd06" => ["fy15_EN-PT_BR_d","fy15_EN-ZH_HANS_c",],
+"mtprd07" => ["fy15_EN-PT_BR_d","fy15_EN-ZH_HANS_c",],
+"mtprd08" => ["fy15_EN-PT_BR_d","fy15_EN-ZH_HANS_c",],
+"mtprd09" => ["fy15_EN-CS_d","fy15_EN-ES_c",],
+"mtprd10" => ["fy15_EN-CS_d","fy15_EN-ES_c",],
+"mtprd11" => ["fy15_EN-CS_d","fy15_EN-ES_c",],
+"mtprd12" => ["fy15_EN-CS_d","fy15_EN-ES_c",],
 );
+
+my $busyList :shared = shared_clone { map {$_ => 0} qw/mtprd01 mtprd02 mtprd03 mtprd04 mtprd05 mtprd06 mtprd07 mtprd08 mtprd09 mtprd10 mtprd11 mtprd12 ussclpdapcmsl01 ussclpdapcmsl02 ussclpdapcmsl03 ussclpdapcmsl04 ussclpdapcmsl05 ussclpdapcmsl06 ussclpdapcmsl07 ussclpdapcmsl08 ussclpdapcmsl09 ussclpdapcmsl10 ussclpdapcmsl11 ussclpdapcmsl12 ussclpdmtlns001 ussclpdmtlns002 ussclpdmtlns003 ussclpdmtlns004 ussclpdmtlns005 ussclpdmtlns006 ussclpdmtlns007 ussclpdmtlns008 ussclpdmtlns009 ussclpdmtlns010 ussclpdmtlns011 ussclpdmtlns012/ };
 
 my $deployments = new Thread::Queue;
 
@@ -82,11 +89,29 @@ my $deploy = sub {
 			print STDOUT threads->tid().": Finished work!\n";
 			last;
 		}
+		my $busy = 0;
+		{ lock $busyList;
+			if ($busyList->{$deployment->{target}}) {
+				$busy = 1;
+			} else {
+				$busyList->{$deployment->{target}} = 1;
+			}
+		}
+		if ($busy) {
+			print STDOUT threads->tid().": ", "Server $deployment->{target} busy. Requeuing engine $deployment->{source}.\n";
+			$deployments->insert(-$threads, $deployment);
+			sleep 10;
+			next;
+		}
 		if (defined $testDrive) {
 			print STDOUT threads->tid().": ", join " ", "rsync", "-azyvv", "/Volumes/LaCie_Work/Autodesk/".$deployment->{source}, "cmsuser@".$deployment->{target}.":/local/cms/.", "\n";
+			sleep .1;
 		} else {
 			print STDOUT threads->tid().": ".($deployments->pending()-$threads)." engines left. Deploying using command: ", join " ", "rsync", "-azyvv", "/Volumes/LaCie_Work/Autodesk/".$deployment->{source}, "cmsuser@".$deployment->{target}.":/local/cms/.", "\n";
 			system "rsync", "-azyvv", "/Volumes/LaCie_Work/Autodesk/".$deployment->{source}, "cmsuser@".$deployment->{target}.":/local/cms/.";
+		}
+		{ lock $busyList;
+			$busyList->{$deployment->{target}} = 0;
 		}
 	}
 };
