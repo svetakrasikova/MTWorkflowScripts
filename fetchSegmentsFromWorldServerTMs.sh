33dead652e5e01506d713ffe77d9bd8bc8798bd9
laeubli <sam@ventzi-adsk.ads.autodesk.com>
2015-01-13 11:49:52 +0100

    run fetchSegmentsFromWorldServer.pl as normal user, not sudo

--- fetchSegmentsFromWorldServerTMs.sh
+++ fetchSegmentsFromWorldServerTMs.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # Wrapper for fetchSegmentsFromAthena.pl
-# Makes sure the Perl script is executable by including pointers to Oracle's ins
-tantclient libraries
+# Makes sure the Perl script is executable by including pointers to Oracle's instantclient libraries
 
 # Usage: fetchSegmentsFromWorldServerTMs.sh [any arguments you'd pass to the respective .pl file]
 
-sudo DYLD_LIBRARY_PATH=/Library/Oracle/instantclient_11_2:$DYLD_LIBRARY_PATH perl fetchSegmentsFromWorldServerTMs.pl $@
+DYLD_LIBRARY_PATH=/Library/Oracle/instantclient_11_2:$DYLD_LIBRARY_PATH perl /OptiBay/ADSK_Software/fetchSegmentsFromWorldServerTMs.pl $@
